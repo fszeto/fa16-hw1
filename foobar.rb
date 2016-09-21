2fe57b8bd32b821c1511826fdf482a916cd34ee2
Faith Szeto <faithszeto@Pauls-MacBook-Pro-2.local>
2016-09-21 12:42:16 -0700

    Push

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    a.map!{ |c| c.to_i + 2 }
+    a = a.values_at(* a.each_index.select {|i| i.even?})
+    a.uniq!
+    [a.inject(:+), 10].min
   end
 end
 
